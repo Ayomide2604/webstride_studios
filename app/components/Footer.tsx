4a9ca63d3e8a5751960e289db3c8b154bb5fc6ab
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-03-03 16:44:56 -0700

    edited footer content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
 							<div className="col-lg-3 col-12">
 								<div className="position-relative">
 									<div className="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0">
-										<h4>Service</h4>
+									<h4>Services</h4>
 										<a
 											className="d-block d-lg-none stretched-link text-body"
 											data-bs-toggle="collapse"
@@ -35,7 +35,7 @@ const Footer = () => {
 													href="index.html"
 													className="text-decoration-none text-reset"
 												>
-													Web App Development
+													Web Design
 												</a>
 											</li>
 											<li className="mb-2">
@@ -43,7 +43,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													Front End Development
+													Web Development
 												</a>
 											</li>
 											<li className="mb-2">
@@ -51,7 +51,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													MVP Development
+													Responsive Design
 												</a>
 											</li>
 											<li className="mb-2">
@@ -59,7 +59,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													Digital Marketing
+													CMS Integration
 												</a>
 											</li>
 											<li className="mb-2">
@@ -67,7 +67,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													Content Writing
+													SEO Optimization
 												</a>
 											</li>
 										</ul>
@@ -77,7 +77,7 @@ const Footer = () => {
 							<div className="col-lg-3 col-12">
 								<div>
 									<div className="mb-3 pb-2 d-flex justify-content-between border-bottom border-bottom-lg-0 position-relative">
-										<h4>About us</h4>
+										<h4>Links</h4>
 										<a
 											className="d-block d-lg-none stretched-link text-body collapsed"
 											data-bs-toggle="collapse"
@@ -100,7 +100,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													Case Studies
+													Projects
 												</a>
 											</li>
 											<li className="mb-2">
@@ -132,7 +132,7 @@ const Footer = () => {
 													href="#!"
 													className="text-decoration-none text-reset"
 												>
-													Career
+													Privacy
 												</a>
 											</li>
 										</ul>
@@ -177,17 +177,17 @@ const Footer = () => {
 												href="changelog.html"
 												className="text-decoration-none text-reset"
 											>
-												Content ful
+												Sanity
 											</a>
 										</li>
 										<li className="mb-2">
 											<a href="#!" className="text-decoration-none text-reset">
-												Vercel
+												Tailwind CSS
 											</a>
 										</li>
 										<li className="mb-2">
 											<a href="#!" className="text-decoration-none text-reset">
-												Netlify
+												TypeScript
 											</a>
 										</li>
 									</ul>
@@ -246,10 +246,14 @@ const Footer = () => {
 					</div>
 					<div className="col-lg-3 col-12">
 						<div className="me-7">
-							<h4 className="mb-4">Headquarters</h4>
-							<p className="text-body-secondary">
-								Codescandy, 412, Residency Rd, Shanthala Nagar, Ashok Nagar,
-								Bengaluru, Karnataka, India 560025
+							<h4 className="mb-4">Contact</h4>
+							<p className="text-body-secondary mb-2">
+								Edmonton, Canada
+							</p>
+							<p className="text-body-secondary mb-0">
+								<a href="tel:+14376855676" className="text-decoration-none text-body-secondary">
+									437-685-5676
+								</a>
 							</p>
 						</div>
 					</div>
@@ -264,14 +268,7 @@ const Footer = () => {
 					</div>
 					<div className="col-md-9 col-lg-6">
 						<div className="small mb-3 mb-lg-0 text-lg-center">
-							Copyright © 2024
-							<span className="text-primary">
-								<a href="#">Block Bootstrap 5 Theme</a>
-							</span>
-							| Designed by
-							<span className="text-primary">
-								<a href="#">CodesCandy</a>
-							</span>
+								Copyright © {new Date().getFullYear()} Edmonton Web Developer. All rights reserved.
 						</div>
 					</div>
 					<div className="col-lg-3">
@@ -366,8 +363,9 @@ const Footer = () => {
 									>
 										<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
 									</svg>
-								</a>
-							</div>
+								</a>							<a href="https://wa.me/14376855676" className="btn btn-whatsapp btn-icon" target="_blank" rel="noopener noreferrer">
+								<i className="bi bi-whatsapp" />
+							</a>							</div>
 						</div>
 					</div>
 				</div>
