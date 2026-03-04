d1d15853865957541f0762757135033f8a2cbc16
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-03-04 16:30:29 -0700

    Added tech stack to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Appointment from "./sections/Appointment";
 import Hero from "./sections/Hero";
 import Services from "./sections/Services";
+import TechStack from "./sections/TechStack";
 
 export default function Home() {
 	return (
 		<div>
 			<Hero />
-			<Services/>
-			<Appointment/>
+			<Services />
+			<TechStack />
+			<Appointment />
 		</div>
 	);
 }
---
+++ app/sections/TechStack.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+
+const TechStack = () => {
+	const techStack = [
+		{ name: "Python", image: "/assets/images/tech/python.svg" },
+		{ name: "JavaScript", image: "/assets/images/tech/javascript.svg" },
+		{ name: "Django", image: "/assets/images/tech/django.svg" },
+		{ name: "Next.js", image: "/assets/images/tech/nextjs.svg" },
+		{ name: "React", image: "/assets/images/tech/react.svg" },
+		{ name: "PostgreSQL", image: "/assets/images/tech/postgres.svg" },
+		{ name: "Node.js", image: "/assets/images/tech/nodejs.svg" },
+		{ name: "GitHub", image: "/assets/images/tech/github.svg" },
+		{ name: "Vercel", image: "/assets/images/tech/vercel.svg" },
+	];
+
+	return (
+		<section className="py-xl-9 py-5 bg-light">
+			<div className="container">
+				<div className="row">
+					<div className="col-xl-6 offset-xl-3 col-md-12 col-12">
+						<div className="text-center mb-xl-7 mb-5" data-cue="fadeIn">
+							<small className="text-uppercase ls-md fw-semibold">
+								modern tech stack
+							</small>
+							<h2 className="my-3">Building with cutting-edge technologies</h2>
+							<p className="mb-0 text-body">
+								We work with proven technologies designed to solve your product
+								requirements. Focus on finding solutions for your business needs
+								while we handle the technical aspect.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-10 offset-lg-1 col-md-12 col-12">
+						<div className="row row-cols-lg-5 row-cols-md-4 row-cols-2 justify-content-center g-5">
+							{techStack.map((tech, index) => (
+								<div className="col" data-cue="slideInDown" key={tech.name}>
+									<figure className="text-center mb-0">
+										<Image
+											src={tech.image}
+											alt={tech.name}
+											width={80}
+											height={80}
+											className="img-fluid"
+											style={{ objectFit: "contain" }}
+										/>
+										<figcaption className="mt-2">
+											<small className="text-muted">{tech.name}</small>
+										</figcaption>
+									</figure>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default TechStack;
