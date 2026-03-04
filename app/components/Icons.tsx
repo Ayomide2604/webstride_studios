5dd17ad210c2b2e1fab368c703c5878037c95577
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-03-04 16:01:31 -0700

    optimized services section

---
+++ app/components/Icons.tsx
@@ -0,0 +1,69 @@
+export const CodeSlashIcon = ({ width = 18, height = 18, className = "" }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width}
+		height={height}
+		fill="currentColor"
+		className={`bi bi-code-slash ${className}`}
+		viewBox="0 0 16 16"
+	>
+		<path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 1 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+	</svg>
+);
+
+export const PaletteIcon = ({ width = 18, height = 18, className = "" }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width}
+		height={height}
+		fill="currentColor"
+		className={`bi bi-palette ${className}`}
+		viewBox="0 0 16 16"
+	>
+		<path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+		<path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z"/>
+	</svg>
+);
+
+export const MegaphoneIcon = ({ width = 18, height = 18, className = "" }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width}
+		height={height}
+		fill="currentColor"
+		className={`bi bi-megaphone ${className}`}
+		viewBox="0 0 16 16"
+	>
+		<path d="M13 2.5a1.5 1.5 0 1 1 3 0v11a1.5 1.5 0 1 1-3 0v-11zm-1 .724c-2.063.665-3.5 2.757-3.5 5.102 0 2.346 1.437 4.438 3.5 5.104v-10.21zM8 4.5a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h3zm0 2a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h3zm0 2a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h3zm0 2a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h3z"/>
+		<path d="M3 5a2 2 0 0 1 2-2h.5a.5.5 0 0 1 0 1H5a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h.5a.5.5 0 0 1 0 1H5a2 2 0 0 1-2-2V5z"/>
+	</svg>
+);
+
+export const ArrowRightIcon = ({ width = 14, height = 14, className = "" }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width}
+		height={height}
+		fill="currentColor"
+		className={`bi bi-arrow-right ${className}`}
+		viewBox="0 0 16 16"
+	>
+		<path
+			fillRule="evenodd"
+			d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+		/>
+	</svg>
+);
+
+export const CheckCircleIcon = ({ width = 16, height = 16, className = "" }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width}
+		height={height}
+		fill="currentColor"
+		className={`bi bi-check-circle-fill ${className}`}
+		viewBox="0 0 16 16"
+	>
+		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+	</svg>
+);
